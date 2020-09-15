890a1bb4ee014a52a546a903b4cc5eef0d260430
David Ka <david.karnel@student.kyh.se>
2020-09-15 13:55:40 +0200

    FindingFiles function completed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,22 +2,33 @@ package com.company;
 
 import java.io.File;
 import java.sql.SQLOutput;
+import java.util.concurrent.ExecutionException;
 
-public class Main {
+class Main {
+
+    public static String userDir;
 
     public static void main(String[] args) {
 	// write your code here
 
-        String userDir = System.getProperty("user.dir");
-
+        String searchWord = "READ";
+        userDir = System.getProperty("user.dir");
         File filesDir = new File(userDir);
+        findFiles(searchWord, filesDir);
 
-        String[] fileNames = filesDir.list();
-        //String[] files = {"ost", "hamburgare", "tacos"};
+    }
 
-        for(String file : fileNames){
-            System.out.println(file);
+    public static void findFiles(String searchWord, File files){
+        File[] fileDirectory = files.listFiles();
+        for(File file : fileDirectory){
+            if(file.isDirectory()){
+                findFiles(searchWord, file);
+            } else {
+                String fileName = file.getName();
+                if (fileName.contains(searchWord)) {
+                        System.out.println(userDir + "\\" + file);
+                }
+            }
         }
-
     }
 }
