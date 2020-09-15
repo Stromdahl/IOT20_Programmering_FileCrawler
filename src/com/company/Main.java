a23067ba293f32667335dc8e808e9043d63598d1
Mattias Strömdahl <Mattias.Strmdahl@student.kyh.com>
2020-09-15 12:24:40 +0200

    progress

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,23 @@
 package com.company;
 
+import java.io.File;
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
-        System.out.println(System.getProperty("user.dir"));
+        String userDir = System.getProperty("user.dir");
 
+        File filesDir = new File(userDir);
+
+        String[] fileNames = filesDir.list();
+        //String[] files = {"ost", "hamburgare", "tacos"};
+
+        for(String file : fileNames){
+            System.out.println(file);
+        }
 
     }
 }
