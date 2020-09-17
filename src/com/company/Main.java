caa3e8319f2adbf50cb004f5e65b0f3fc9b9bcee
Besim <Besim.musliu@student.kyh.se>
2020-09-17 17:04:08 +0200

    Added new method (fileContains)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,13 @@
 package com.company;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.SQLOutput;
 import java.util.concurrent.ExecutionException;
+import java.util.Scanner;
 
 class Main {
-
     public static String userDir;
-
     public static void main(String[] args) {
 	// write your code here
 
@@ -17,6 +17,21 @@ class Main {
         findFiles(searchWord, filesDir);
 
     }
+    public static boolean fileContains(File file, String searchWord){
+        try{
+            Scanner fileScanner = new Scanner(file);
+            while(fileScanner.hasNextLine()){
+                String line = fileScanner.nextLine();
+                if(line.contains(searchWord))
+                {
+                    return true;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println(file.getAbsolutePath());
+        }
+        return false;
+    }
 
     public static void findFiles(String searchWord, File files){
         File[] fileDirectory = files.listFiles();
@@ -25,6 +40,7 @@ class Main {
                 findFiles(searchWord, file);
             } else {
                 String fileName = file.getName();
+                
                 if (fileName.contains(searchWord)) {
                         System.out.println(userDir + "\\" + file);
                 }
